b30ab27158811fcb147e92bb4f6445b326eafb76
David Dellanave <david@dellanave.com>
2012-12-28 01:02:13 -0600

    added option and set default to false for defunct browscap

--- genetify/CONFIG.php
+++ genetify/CONFIG.php
@@ -5,7 +5,7 @@ $DB['user'] = 'genetify_user';
 $DB['password'] = '';
 $DB['database'] = 'genetify';
 
-$CONFIG['browsecap'] = false; // browscap is defunct, turn off by default if & until a replacement is installed
+$CONFIG['browscap'] = false; // browscap is defunct, turn off by default if & until a replacement is installed
 
 // change as desired for debugging
 error_reporting(E_ALL);
